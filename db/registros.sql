de5290ef91a1b916efa375284ae419a3dc54f15c
alexis14kl <rapalexism@gmail.com>
2022-03-05 15:31:46 -0500

    ok

--- db/registros.sql
+++ db/registros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-03-2022 a las 23:36:28
+-- Tiempo de generación: 05-03-2022 a las 21:31:06
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `registros`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datosgenerales`
+--
+
+CREATE TABLE `datosgenerales` (
+  `iD_Datos_Generales` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL,
+  `cod_cliente` int(8) NOT NULL,
+  `tipo_documento` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Numero_cc` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Telefono` varchar(13) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `datosgenerales`
+--
+
+INSERT INTO `datosgenerales` (`iD_Datos_Generales`, `idusuario`, `cod_cliente`, `tipo_documento`, `Numero_cc`, `direccion`, `Telefono`) VALUES
+(15, 15, 415436, 'no', 'no ', 'no', 'no'),
+(16, 16, 766307, 'no', 'no ', 'no', 'no');
 
 -- --------------------------------------------------------
 
@@ -56,8 +80,8 @@ CREATE TABLE `usuarioperfil` (
 --
 
 INSERT INTO `usuarioperfil` (`idusuario`, `idperfil`) VALUES
-(48, 1),
-(49, 2);
+(15, 1),
+(16, 1);
 
 -- --------------------------------------------------------
 
@@ -79,12 +103,19 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `Nombre_completo`, `username`, `password`, `estatus`, `conect`) VALUES
-(48, 'yilson alexis malambo madrigal', 'rapalexism@gmail.com', '{noop}123', 1, 1),
-(49, 'daira', 'daira@gmail.com', '{noop}123', 1, 1);
+(15, 'dfd', 'daira@gmail.com', '{noop}123', 1, 0),
+(16, 'yilson', 'rapalexism@gmail.com', '{noop}123', 1, 0);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `datosgenerales`
+--
+ALTER TABLE `datosgenerales`
+  ADD PRIMARY KEY (`iD_Datos_Generales`),
+  ADD KEY `idusuario` (`idusuario`);
 
 --
 -- Indices de la tabla `perfiles`
@@ -110,6 +141,12 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `datosgenerales`
+--
+ALTER TABLE `datosgenerales`
+  MODIFY `iD_Datos_Generales` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- AUTO_INCREMENT de la tabla `perfiles`
 --
 ALTER TABLE `perfiles`
@@ -119,11 +156,17 @@ ALTER TABLE `perfiles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `datosgenerales`
+--
+ALTER TABLE `datosgenerales`
+  ADD CONSTRAINT `datosgenerales_ibfk_1` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `usuarioperfil`
